5fce886904495e478cba94bb54862af0cafdff22
lblueter <logan11023@yahoo.com>
2018-11-03 10:28:09 -0400

    add packages and build db

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Hamburger",false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheeseburger",false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Veggieburger",false);
