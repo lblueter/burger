d39733561f6a16203c862107d39da4c67c94c5a6
lblueter <logan11023@yahoo.com>
2018-11-06 16:57:14 -0500

    complete file structure

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,5 @@
+var express = require('express')
+var router = express.Router();
+var burgers = require('../models/burgers')
+
+module.exports = router
---
+++ models/burgers.js
@@ -0,0 +1,7 @@
+var orm = require('../config/orm')
+
+var burgers = {
+
+}
+
+module.exports = burgers
