672a12610eaaef42d788d2b16197aa42dbd0389b
lblueter <logan11023@yahoo.com>
2018-11-06 19:28:19 -0500

    fixed error and misnaming

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
